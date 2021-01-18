4ecf58f486f4388bcbf7a1368798cc86ed485314
tenzyu <tenzyu.on@gmail.com>
2021-01-18 21:42:13 +0900

    refactoring

--- launcher.py
+++ launcher.py
@@ -1,11 +1,9 @@
-from glob import glob
+import glob
 from os import getenv
+from pathlib import Path
 from traceback import print_exc
 
 from discord.ext import commands
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 class MyBot(commands.Bot):
@@ -14,24 +12,27 @@ class MyBot(commands.Bot):
         print("Starting Simple Quote...")
         self.remove_command("help")
 
-        for cog in [cog.replace("/", ".").replace(".py", "") for cog in glob("cogs/*.py")]:
+        for cog in Path("cogs/").glob("*.py"):
             try:
-                self.load_extension(cog)
-                print(f"loaded: {cog}")
-            except BaseException:
+                self.load_extension("cogs." + cog.stem)
+                print(f"Loaded {cog.stem}.py")
+            except:
                 print_exc()
 
     async def on_ready(self):
-        user = self.user
-        print("logged in as:", str(user))
+        print("logged in as:", self.user.name, self.user.id)
 
     async def on_command_error(self, ctx, error):
-        ignore_error = (commands.CommandNotFound, commands.BadArgument, commands.CheckFailure)
-        if isinstance(error, ignore_error):
+        ignore_errors = (
+            commands.CommandNotFound,
+            commands.BadArgument,
+            commands.CheckFailure,
+        )
+        if isinstance(error, ignore_errors):
             return
         await ctx.send(error)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bot = MyBot()
     bot.run(getenv("DISCORD_BOT_TOKEN"))
