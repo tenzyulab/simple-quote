63d59a85bbec325209a9fbe8497001b703b1c6e8
tenzyu <tenzyu.on@gmail.com>
2021-01-18 23:24:28 +0900

    Extend quote.py
    supported Send copied embeds, NSFW to SFW and Image

--- cogs/quote.py
+++ cogs/quote.py
@@ -11,13 +11,17 @@ class Quote(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def send_copy_embeds(self, embeds, channel):
+        for embed in embeds:
+            await channel.send(embed=embed)
+
     async def create_embed(self, message):
         author = message.author
         embed = discord.Embed(description=message.content,
                               timestamp=message.created_at)
         embed.set_author(name=author.display_name,
-                         url=message.jump_url,
-                         icon_url=author.avatar_url)
+                         icon_url=author.avatar_url,
+                         url=message.jump_url)
         embed.set_footer(text="#" + message.channel.name)
         return embed
 
@@ -26,20 +30,37 @@ class Quote(commands.Cog):
         if message.author.bot:
             return
 
-        response = pattern.finditer(message.content)
-        for match_url in response:
+        for match_url in pattern.finditer(message.content):
             if message.guild.id != int(match_url.group("guild_id")):
                 continue
             channel = self.bot.get_channel(int(match_url.group("channel_id")))
             quoted = await channel.fetch_message(int(match_url.group("message_id")))
-            embed = await self.create_embed(quoted)
-            if quoted.attachments:
-                embed.set_image(url=quoted.attachments[0].url)
-            await message.channel.send(embed=embed)
-            if quoted.embeds:
-                for quoted_embed in quoted.embeds:
-                    await message.channel.send(embed=quoted_embed)
 
+            # If only Embeds are quoted
+            if len(quoted.content) == 0 and quoted.embeds:
+                await self.send_copy_embeds(quoted.embeds, channel)
+                continue
+
+            # If quoted from NSFW to SFW
+            if channel.is_nsfw() and not message.channel.is_nsfw():
+                quoted.content = "**Quoted from NSFW**\nclick to view messageUrl"
+                quoted.attachments = None
+            embed = await self.create_embed(quoted)
+
+            # if quoted Image
+            fixed_file = None
+            if quoted.attachments:
+                if quoted.attachments[0].is_spoiler():
+                    fixed_file = await quoted.attachments[0].to_file(spoiler=True)
+                else:
+                    embed.set_image(url=quoted.attachments[0].url)
+            
+            await message.channel.send(embed=embed, file=fixed_file)
+
+            # If embeds are also quoted
+            if quoted.embeds:
+                await message.channel.send("──────────")
+                await self.send_copy_embeds(quoted.embeds, channel)
 
 def setup(bot):
     bot.add_cog(Quote(bot))
