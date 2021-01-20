40804fdfee3a97d15a2f0e98d84ad3e7361b3782
tenzyu <tenzyu.on@gmail.com>
2021-01-20 23:59:33 +0900

    refactoring

--- cogs/quote.py
+++ cogs/quote.py
@@ -12,6 +12,10 @@ class Quote(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def send_copied_embeds(self, embeds, channel):
+        for embed in embeds:
+            await channel.send(embed=embed)
+
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot:
@@ -35,37 +39,41 @@ class Quote(commands.Cog):
                 await message.channel.send(embed=embed)
                 continue
 
-            embed = None
-            if len(quoted.content) != 0:
-                author = message.author
-                embed = discord.Embed(
-                    description=quoted.content, timestamp=quoted.created_at
-                )
-                embed.set_author(
-                    name=author.display_name,
-                    icon_url=author.avatar_url,
-                    url=quoted.jump_url,
-                )
-                embed.set_footer(text="#" + quoted.channel.name)
+            # If no messages
+            if len(quoted.content) == 0:
+                if quoted.attachments:
+                    if quoted.attachments[0].is_spoiler():
+                        fixed_file = await quoted.attachments[0].to_file(spoiler=True)
+                        await message.channel.send(file=fixed_file)
+                    else:
+                        embed = discord.Embed()
+                        embed.set_image(url=quoted.attachments[0].url)
+                        await message.channel.send(embed=embed)
+                if quoted.embeds:
+                    await self.send_copied_embeds(quoted.embeds, message.channel)
+                continue
+
+            embed = discord.Embed(
+                description=quoted.content, timestamp=quoted.created_at
+            )
+            embed.set_author(
+                name=quoted.author.display_name,
+                icon_url=quoted.author.avatar_url,
+                url=quoted.jump_url,
+            )
 
             fixed_file = None
             if quoted.attachments:
                 if quoted.attachments[0].is_spoiler():
                     fixed_file = await quoted.attachments[0].to_file(spoiler=True)
                 else:
-                    if not embed:  # If only the image is quoted
-                        embed = discord.Embed()
                     embed.set_image(url=quoted.attachments[0].url)
-            try:
-                await message.channel.send(embed=embed, file=fixed_file)
-            except:
-                pass
+
+            await message.channel.send(embed=embed, file=fixed_file)
 
             if quoted.embeds:
-                if embed:  # If not only Embeds are quoted
-                    await message.channel.send("──────────")
-                for embed in quoted.embeds:
-                    await message.channel.send(embed=embed)
+                await message.channel.send("────────")
+                await self.send_copied_embeds(quoted.embeds, message.channel)
 
 
 def setup(bot):
